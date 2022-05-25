70c55a7925d6fd2f270866ff477499d45936ed64
Szymon Łukaszyk <szymon@patent.pl>
2022-05-25 18:55:48 +0200

    Add files via upload

--- ballsfg.m
+++ ballsfg.m
@@ -13,6 +13,7 @@ function [num den res] = ballsfg(kind, n)
 % History
 % v1: 21.05.2022
 % v2: 22.05.2022
+% v3: 25.05.2022 % sign factor as integral powers of the imaginary unit simplifications
 
 if nargin ~= 2
     error 'Wrong number of arguments';
@@ -22,8 +23,7 @@ if mod(n,1)
     error 'n is not an iteger';
 end
 
-floorn2     = floor(n/2);
-signfct     = -( (-1)^floorn2 );
+%floorn2     = floor(n/2);
 
 num = 0;
 den = 1;
@@ -31,20 +31,20 @@ res = 0;
 
 switch kind
   case 'f'
-    if ~mod(n,2)  % n is even
+    if ~mod(n,2)   % n is even
       if n>=0
         num = 1;
         den = factorial(n/2);        
       end
     else           % n is odd
-      if n>0
+      if n>=-1
         num = ( 2^(n+1) ) * factorial( (n+1)/2 );
         den = factorial(n+1);
         nn  = (n+1)/2; % simplify the fraction
         fct = factorial(nn)*2^nn;
         num = num/fct;
         den = den/fct;        
-     else
+     else %n<=-1
         num = factorial(-n-1);
         den = factorial( (-n-1)/2 )*2^(-n-1);
         nn  = -(n+1)/2; % simplify the fraction
@@ -54,25 +54,38 @@ switch kind
       end
     end
   case 'g'
-    if ~mod(n,2)  % n is even
+    if ~mod(n,2)   % n is even
       if n>=0
         num = 1;
-        den = (4^floorn2)*factorial(floorn2);
+%        den = (4^floorn2)*factorial(floorn2);
+        den = factorial(n/2)*2^n;
       end 
     else           % n is odd
-      if n>0
-         num = 1;
-         den = factorial(2*floorn2+1)/factorial(floorn2);
-      else
-        num = 2*factorial( 2*floor(-n/2) );
-        den = factorial( floor(-n/2) );  
+      if n>=1
+%         num = 1;
+%         den = factorial(2*floorn2+1)/factorial(floorn2);
+         num = factorial( (n-1)/2 );
+         den = factorial(n);
+         % simplify the fraction
+         den = den/num;        
+         num = num/num;
+      else %n<=1
+%        num = 2*factorial( 2*floor(-n/2) );
+%        den = factorial( floor(-n/2) );  
+        num = 2*factorial( -n-1 );
+        den = factorial( (-n-1)/2 );  
+        % simplify the fraction
+        num = num/den;
+        den = den/den;        
       end
     end
   otherwise
-    error 'Unknown kind of relation';
+    error 'Unknown kind of recurrence relation';
 end
 
 if n<0
+%  signfct     = -( (-1)^floorn2 );
+  signfct     = -i^(n-1);
   res = signfct*num/den;
 else  
   res = num/den;
